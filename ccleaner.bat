932ef0e5a115a45b2ba43c2b9d15a4e3c31b1edf
Juan Jose Pablos <juanjo@apertus.es>
2013-09-14 01:02:30 +0200

    remove link from desktop add new version 4.0.5

--- ccleaner.bat
+++ ccleaner.bat
@@ -1,6 +1,6 @@
 ::OPTIONAL: Install Ccleaner
 ::HOME: http://www.piriform.com/CCLEANER
-::URL|ALL|http://download.piriform.com/ccsetup326.exe|packages/ccsetup326.exe
 
-ccsetup401.exe /S
+ccsetup405.exe /S
 "%ProgramFiles%\CCleaner\CCleaner.exe" /AUTO
+del "%allusersprofile%\Escritorio\CCleaner.lnk"
