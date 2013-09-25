08b589b59ecf1e2148e12090ca555cf0bde9253c
Juan Jose Pablos <juanjo@apertus.es>
2013-09-25 14:55:49 +0200

    update to version 4.1.1

--- libreoffice.bat
+++ libreoffice.bat
@@ -2,4 +2,4 @@
 ::HOME: http://www.libreoffice.org/
 
 
-msiexec /qn /l* %TEMP%\libreoffice.txt /i libreoffice_4.0.3_win_x86_install.msi /norestart /passive
+msiexec /qn /l* %TEMP%\libreoffice.txt /i libreoffice_4.1.1_win_x86_install.msi /norestart /passive
