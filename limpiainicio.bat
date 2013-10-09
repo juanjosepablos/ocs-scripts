c57a621f3df4a3a6d2c6dc94a7cbcd5b8650d8d1
Juan Jose Pablos <juanjo@apertus.es>
2013-10-09 18:22:51 +0200

    add some programs

--- limpiainicio.bat
+++ limpiainicio.bat
@@ -1,2 +1,8 @@
-#Ares
+::Ares
 reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v ares
+::Messenger
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v MsnMsgr
+::Google Notifier
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v swg
+::Nokia Suite
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "PC Suite Tray"
