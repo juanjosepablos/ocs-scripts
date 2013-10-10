37e854cd62db451f50399fcdf8a486a82dc25db3
Juan Jose Pablos <juanjo@apertus.es>
2013-10-10 03:04:19 +0200

    no instalation dialogs

--- perl.bat
+++ perl.bat
@@ -1,4 +1,4 @@
 :: HOME: http://downloads.activestate.com/ActivePerl/releases/
 
-msiexec /qb /i activeperl-5.14.2.1402-mswin32-x86-295342.msi PERL_PATH=Yes PERL_EXT=Yes TARGETDIR="%ProgramFiles%" ADDLOCAL="PERL_FEATURE"
+msiexec /qn /i activeperl-5.14.2.1402-mswin32-x86-295342.msi PERL_PATH=Yes PERL_EXT=Yes TARGETDIR="%ProgramFiles%" ADDLOCAL="PERL_FEATURE"
 
