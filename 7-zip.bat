98ae27bdc388e21dfadaed448f51ae039d02b816
Juan Jose Pablos <juanjo@apertus.es>
2013-10-10 03:02:18 +0200

    No instalation dialogs.

--- 7-zip.bat
+++ 7-zip.bat
@@ -1,4 +1,4 @@
 :: OPTIONAL: Install 7-Zip^M
 :: HOME: http://sevenzip.sourceforge.net/
 
-msiexec /qb /i 7z920-%PROCESSOR_ARCHITECTURE%.msi REBOOT=ReallySuppress
+msiexec /qn /i 7z920-%PROCESSOR_ARCHITECTURE%.msi REBOOT=ReallySuppress
