2f46753b35c177ceff4f61dcc864f0d6e3b7c28d
Juan Jose Pablos <juanjo@apertus.es>
2013-10-14 19:23:20 +0200

    Several version of Openoffice StarOffice to remove

---
+++ openoffice-uninstall.bat
@@ -0,0 +1,42 @@
+::Remove OpenOffice
+
+::OpenOffice.org 2.3 
+msiexec /qn /norestart /x {138049E9-E220-4410-BF0C-B35D26FF9EBD}
+msiexec /qn /norestart /x {83C03FBE-4492-4133-BBAB-421CD88ADA32}
+
+::OpenOffice.org 2.4 
+msiexec /qn /norestart /x {F87A8E11-02A4-4875-A3A5-5961081B0E4E}
+msiexec /qn /norestart /x {2BB8FBB4-CFF9-434E-AA0A-40F5379C1602}
+
+:: OpenOffice 3.0 / 3.0.1
+msiexec /qn /norestart /x {E42E4162-46B9-43BC-9FE2-1BBE772CC494}
+msiexec /qn /norestart /x {92B79901-C57D-409F-8D2F-4E5337383569}
+msiexec /qn /norestart /x {F72EACD9-3ED6-41D9-A49D-E06542992B9F}
+msiexec /qn /norestart /x {6860B340-530D-46B3-91F8-1AE1F70F7C33}
+
+:: OpenOffice 3.1.0 / 3.1.1
+msiexec /qn /norestart /x {A16B3EA2-8798-4960-8D8B-18D3149AD617}
+msiexec /qn /norestart /x {AFB21BFE-40E6-42FB-A010-B80F417D8E71}
+msiexec /qn /norestart /x {43A650AA-D1DC-4C52-8819-D7848B3A08DA
+
+:: OpenOffice 3.1.1
+msiexec /qn /norestart /x {D765F1CE-5AE5-4C47-B134-AE58AC474740}
+msiexec /qn /norestart /x {A01E0232-CA9E-4FA2-BAA1-39358D753353}
+
+:: OpenOffice 3.2.0-12
+msiexec /qn /norestart /x {CC5CC633-D711-48A4-A812-9ED64BC47009}
+msiexec /qn /norestart /x {60CE6B15-E8DC-4096-83FA-5D8DE8B9ED5B}
+msiexec /qn /norestart /x {899BA88D-139C-4247-BE38-45C9B3981B92}
+msiexec /qn /norestart /x {97B3824E-B2D2-4C49-A860-BCA56F10B040}
+msiexec /qn /norestart /x {DFFC0648-BC4B-47D1-93D2-6CA6B9457641}
+
+:: OpenOffice.org 3.3 
+msiexec /qn /norestart /x {3E171899-0175-47CC-84C4-562ACDD4C021}
+
+:: OpenOffice.org 3.4 
+msiexec /qn /norestart /x {7DA1C06F-C913-46C7-8A0F-DA2CBA17EA1D}
+
+::StarOffice 8
+msiexec /qn /norestart /x {4BC1CB2B-FDCE-4DB4-A557-BA8127569B0D}
+msiexec /qn /norestart /x {9510AB97-A36C-4352-8725-E72E5528FA1B}
+
