42f8ff18e8002843bfac21f93b0b727107c84032
Juan Jose Pablos <juanjo@apertus.es>
2013-10-14 20:36:08 +0200

    no output when installing panda

--- pandacloud.bat
+++ pandacloud.bat
@@ -4,6 +4,6 @@ del /Q /S %TEMP%\PandaCloud
 
 "%PROGRAMFILES%\7-Zip\7z" x -y -o"%TEMP%\PandaCloud" cloudantivirus-2.1.1.exe
 
-%TEMP%\PandaCloud\PandaCloudAntivirus_x86.msi /qb ANALISISDISABLE=1 INSTALLDIR="%PROGRAMFILES%\Panda Security\Panda Cloud Antivirus\"  ProductLanguage=1034 ALLOWINFOPANDA=true REBOOT=r SETUPEXEDIR=%TEMP%\PandaCloud\ CURRENTDIRECTORY=%TEMP%\PandaCloud
+%TEMP%\PandaCloud\PandaCloudAntivirus_x86.msi /qn ANALISISDISABLE=1 INSTALLDIR="%PROGRAMFILES%\Panda Security\Panda Cloud Antivirus\"  ProductLanguage=1034 ALLOWINFOPANDA=true REBOOT=r SETUPEXEDIR=%TEMP%\PandaCloud\ CURRENTDIRECTORY=%TEMP%\PandaCloud
 
 
