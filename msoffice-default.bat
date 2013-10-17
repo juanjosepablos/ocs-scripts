5e8ad1d7ee896683f2558689f8bcaab091bc8b30
Juan Jose Pablos <juanjo@apertus.es>
2013-10-17 23:08:31 +0200

    splipt office 2003 and office 2007

---
+++ msoffice-default.bat
@@ -0,0 +1,11 @@
+::OFFICE 2003
+SET OFFICE11_HOME=%ProgramFiles%\Microsoft Office\OFFICE11
+IF EXIST "%OFFICE11_HOME%\winword.exe"  "%OFFICE11_HOME%\winword.exe" /r
+IF EXIST "%OFFICE11_HOME%\excel.exe"    "%OFFICE11_HOME%\excel.exe" /regserver
+IF EXIST "%OFFICE11_HOME%\powerpnt.exe" "%OFFICE11_HOME%\excel.exe" /regserver
+
+::OFFICE 2007
+SET OFFICE12_HOME=%ProgramFiles%\Microsoft Office\OFFICE12
+IF EXIST "%OFFICE12_HOME%\winword.exe"  "%OFFICE12_HOME%\winword.exe" /r
+IF EXIST "%OFFICE12_HOME%\excel.exe"    "%OFFICE12_HOME%\excel.exe" /regserver
+IF EXIST "%OFFICE12_HOME%\powerpnt.exe" "%OFFICE12_HOME%\excel.exe" /regserver
