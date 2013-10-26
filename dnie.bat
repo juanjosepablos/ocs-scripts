dc47bfa5ea7191c7a80f0cfe076ae5738b37d2a5
Juan Jose Pablos <juanjo@apertus.es>
2013-10-27 01:48:15 +0200

    + dnie.bat

---
+++ dnie.bat
@@ -0,0 +1,9 @@
+:: OPTIONAL: DNIE version 6.0.2
+:: HOME:http://www.dnielectronico.es/descargas/windows.html
+
+@echo off
+
+DNIe_v6.0.2.msi /qn
+
+:: Uninstall String
+::MsiExec.exe /X {0EEEC9BE-0571-4AD9-9F5F-2957EA414D3C} /qn
