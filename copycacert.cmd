fef17270f645fb9538d79f75db0ba6c2092ca497
Juan Jose Pablos <juanjo@apertus.es>
2013-11-09 01:43:11 +0100

    works with windows 7

--- copycacert.cmd
+++ copycacert.cmd
@@ -1,8 +1,12 @@
 :: Copy cacert
 
 SET CACERT_PATH=cacert.pem
-SET OCS_APPDATA="%ALLUSERSPROFILE%\Datos de programa\OCS Inventory NG\Agent"
+IF NOT EXIST "%ALLUSERSPROFILE%\Datos de programa" GOTO seven
+ SET OCS_APPDATA="%ALLUSERSPROFILE%\Datos de programa\OCS Inventory NG\Agent"
 
+
+:seven
+SET OCS_APPDATA="%ALLUSERSPROFILE%\OCS Inventory NG\Agent"
 
 :: First check if file has been copied already
 
