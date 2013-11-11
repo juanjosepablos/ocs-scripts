728f1dc7d87153852d2ca2c8648aa871fbb3e01f
Juan Jose Pablos <juanjo@apertus.es>
2013-11-11 01:21:39 +0100

    move files to own directory

--- limpiainicio.bat
+++
@@ -1,8 +0,0 @@
-::Ares
-reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v ares
-::Messenger
-reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v MsnMsgr
-::Google Notifier
-reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v swg
-::Nokia Suite
-reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "PC Suite Tray"
