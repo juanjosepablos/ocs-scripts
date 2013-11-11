5922007713b3b8cba6ceea1d13d30aa302c28ec8
Juan Jose Pablos <juanjo@apertus.es>
2013-11-11 01:46:02 +0100

    merge limpiamierda and limpiamierda7

--- limpiamierda/limpiamierda.bat
+++ limpiamierda/limpiamierda.bat
@@ -3,8 +3,13 @@ taskkill /F /IM Firefox.exe /IM iexplore.exe
 
 start ask-toolbar.bat
 start avg-toolbar.bat
+::Babylon
+MsiExec.exe /qn /norestart /x {83AA2913-C123-4146-85BD-AD8F93971D39}
+FOR /D %%V IN ("%PROGRAMFILES%\BabylonToolbar\*") DO %%V\uninstall.exe
+"%PROGRAMFILES%\Babylon\Babylon-Pro\Utils\uninstbb.exe"
+%SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {E55E7026-EF2A-4A17-AAA7-DB98EA3FD1B1}
 ::BitGuard
-start bitguard.bat 
+start bitguard.bat
 ::Bing
 MsiExec.exe /qn /norestart /x{C28D96C0-6A90-459E-A077-A6706F4EC0FC}
 ::Bueno Search
@@ -13,15 +18,11 @@ MsiExec.exe /qn /norestart /x{C28D96C0-6A90-459E-A077-A6706F4EC0FC}
 "%ALLUSERSPROFILE%\Datos de programa\BrowserDefender\2.6.1339.144\{c16c1ccb-7046-4e5c-a2f3-533ad2fec8e8}\uninstall.exe" /Uninstall /{15D2D75C-9CB2-4efd-BAD7-B9B4CB4BC693} /su=396633eb8dd13274 /um
 ::Browser Protect
 "%ALLUSERSPROFILE%\Datos de programa\BrowserProtect\2.6.1125.80\{c16c1ccb-7046-4e5c-a2f3-533ad2fec8e8}\uninstall.exe" /Uninstall /{15D2D75C-9CB2-4efd-BAD7-B9B4CB4BC693} /su=693137bede806a27 /um
+"%ALLUSERSPROFILE%\BrowserProtect\2.6.1125.80\{c16c1ccb-7046-4e5c-a2f3-533ad2fec8e8}\uninstall.exe" /Uninstall /{15D2D75C-9CB2-4efd-BAD7-B9B4CB4BC693} /su=693137bede806a27 /um
 ::BrowserCompanion
 "%PROGRAMFILES%\BrowserCompanion\uninstall.exe"
 ::Browser Manager
 "%ProgramData%\Browser Manager\2.6.1339.144\{16cdff19-861d-48e3-a751-d99a27784753}\uninstall.exe"
-::Babylon
-MsiExec.exe /qn /norestart /x {83AA2913-C123-4146-85BD-AD8F93971D39}
-FOR /D %%V IN ("%PROGRAMFILES%\BabylonToolbar\*") DO %%V\uninstall.exe
-"%PROGRAMFILES%\Babylon\Babylon-Pro\Utils\uninstbb.exe"
-%SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {E55E7026-EF2A-4A17-AAA7-DB98EA3FD1B1}
 ::Bubble
 "%PROGRAMFILES%\Bubble_Dock_Toolbar_ES.B\uninstall.exe" toolbar
 ::ConduitEngine
@@ -40,6 +41,10 @@ FOR /D %%V IN ("%PROGRAMFILES%\Delta\delta\*") DO %%V\uninstall.exe -uprtc /tbGe
 "%PROGRAMFILES%\DealPly\uninst.exe" 
 ::Driver Detective
 %SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {4640FDE1-B83A-4376-84ED-86F86BEE2D41}
+::FACEBOOK MOODS
+FOR /D %%V IN ("%PROGRAMFILES(x86)%\facemoods.com\facemoods\*") DO %%V\uninstall.exe
+::FACEBOOK MOODS
+FOR /D %%V IN ("%PROGRAMFILES%\facemoods.com\facemoods\*") DO %%V\uninstall.exe
 ::FileOpener
 "%PROGRAMFILES%\Tweaks\FileOpener\uninstall.exe" /s
 ::GamingWonderland Toolbar
@@ -50,27 +55,23 @@ start google-toolbar.bat
 ::IB Updater Service
 "C:\Windows\system32\WNLT\Installation\uninstaller.exe"
 ::Iminent
-MsiExec.exe /qn /norestart /x {4BD8E034-E0F4-4509-A753-467A8E854CD8}
-MsiExec.exe /qn /norestart /x {5CDCDBCD-119A-4AE1-9C55-B816DBBE4245}
-MsiExec.exe /qn /norestart /x {81FCC50B-950F-4063-8E4A-D99CAA4FBB1F}
-MsiExec.exe /qn /norestart /x {973DD1DF-D51D-46BB-B6AC-D56617D133C1}
-MsiExec.exe /qn /norestart /x {A76AA284-E52D-47E6-9E4F-B85DBF8E35C3}
-:: IncrediBar
+star iminent-toolbar.bat
+::IncrediBar
 "%PROGRAMFILES%\IB Updater\unins000.exe" /VERYSILENT
-::FACEBOOK MOODS
-FOR /D %%V IN ("%PROGRAMFILES%\facemoods.com\facemoods\*") DO %%V\uninstall.exe
-::MyPC Backup
-"%PROGRAMFILES%\MyPC Backup\uninst.exe"
-::Movistar
-MsiExec.exe /qn /norestart /x {689FCC19-5582-4D88-BDC6-490EB7DAFB82}
-::msn toolbar
-%SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {4BEB7BEF-6A3C-4A4A-A0C5-8A8E8F07ED1C}
+::LyricsSay
+"%PROGRAMFILES(x86)%\LyricsSay-1\Uninstall.exe" /fromcontrolpanel=1
 ::MediaBar
 "%PROGRAMFILES%\Lphant Applications\MediaBar\uninstall.exe"
 "%PROGRAMFILES%\Lphant\unins000.exe" /SILENT
 ::Messenget Plus Smartbar
 %SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {8C267246-FA45-41D4-B1EA-9EB238C6D0FB}
-:: OfferBox
+::Movistar
+MsiExec.exe /qn /norestart /x {689FCC19-5582-4D88-BDC6-490EB7DAFB82}
+::msn toolbar
+%SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {4BEB7BEF-6A3C-4A4A-A0C5-8A8E8F07ED1C}
+::MyPC Backup
+"%PROGRAMFILES%\MyPC Backup\uninst.exe"
+::OfferBox
 "%PROGRAMFILES%\Offerbox\uninstaller.exe" /s
 ::Optimizer Pro
 "%PROGRAMFILES%\Optimizer Pro\unins000.exe" /VERYSILENT
@@ -82,12 +83,13 @@ MsiExec.exe /qn /norestart  /X {199624B4-6BC0-48C2-AB7E-9AB90B249CD7}
 "%PROGRAMFILES%\PricePeep\uninstall.exe" /s
 ::Regclean Pro
 "%PROGRAMFILES%\RegClean Pro\unins000.exe" /VERYSILENT
-::Software Plate
-"%PROGRAMFILES%\Software Plate\uninstall.exe"
 ::SHARES Toolbar
 "%PROGRAMFILES%\shARES\UNWISE.exe" /U "%PROGRAMFILES%\shARES\INSTALL.LOG"
+::Software Plate
+"%PROGRAMFILES%\Software Plate\uninstall.exe"
 ::Speedupmypc
 "%PROGRAMFILES%\Uniblue\SpeedUpMyPC\unins000.exe" /VERYSILENT
+"%PROGRAMFILES%\Acelerar el PC\unins000.exe" /SILENT
 ::SearchProtect
 "%PROGRAMFILES%\SearchProtect\bin\uninstall.exe" /s
 ::SoftwareUpdater
@@ -119,6 +121,9 @@ FOR /D %%V IN ("%PROGRAMFILES%\tutoriales*") DO "%%V\unins001.exe" /SILENT
 %SYSTEMROOT%\SYSTEM32\MsiExec.exe /qn /norestart /x {EBE677C0-CBCB-4EBF-8098-E27E1B5271CF}
 ::Vuze ToolBar
 "%PROGRAMFILES%\Vuze_Remote\uninstall.exe" /s
+::Web Cake
+FOR /D %%V IN ("%ALLUSERSPROFILE%\Datos de programa\Tarma Installer\*") DO %%V\setup.exe /remove /q0
+FOR /D %%V IN ("%ALLUSERSPROFILE%\Tarma Installer\*") DO %%V\setup.exe /remove /q0
 ::Wajam
 "%PROGRAMFILES%\Wajam\uninstall.exe" /s
 ::Yahoo Toolbar
