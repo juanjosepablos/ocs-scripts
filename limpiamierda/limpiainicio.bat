bda9c0ab01b8bbfc814fa3d272cf402e989ace5b
Juan Jose Pablos <juanjo@apertus.es>
2013-11-11 01:20:08 +0100

    move files to an own directory

---
+++ limpiamierda/limpiainicio.bat
@@ -0,0 +1,12 @@
+::Ares
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v ares
+::Messenger
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v MsnMsgr
+::Google Notifier
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v swg
+::Google Update
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "Google Update"
+::Nero Check
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "NeroFilterCheck"
+::Nokia Suite
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "PC Suite Tray"
