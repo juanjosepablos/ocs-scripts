fdd14c09e64a971ce9eec68cea468a8510339b39
Juan Jose Pablos <juanjo@apertus.es>
2013-11-13 12:55:06 +0100

    uninstall all the MS live stuff

---
+++ uninstall/live-uninstall.bat
@@ -0,0 +1,23 @@
+::windows live 
+MsiExec.exe /qn /x {2B83A043-BA8C-4164-98AA-29529D0BE756}
+
+:: Windows live carga
+MsiExec.exe /qn /x {205C6BDD-7B73-42DE-8505-9A093F35A238}
+
+::toolbar
+MsiExec.exe /qn /x {21DD56B6-A7C1-4EA6-BC53-28A63A4A1820}
+
+::Live communications
+MsiExec.exe /qn /x {3175E049-F9A9-4A3D-8F19-AC9FB04514D1}
+
+:: asistente inicio
+MsiExec.exe /qn /x {7593234B-2AEB-4FC9-B02D-C9B30D86084C}
+
+::proteccion
+MsiExec.exe /qn /x {84E6A538-D3AE-4510-B32F-2415361D2770}
+
+::live call
+MsiExec.exe /qn /x {8924FD04-AFF1-4387-B08B-6A979485F2BD}
+::messenger
+MsiExec.exe /qn /x {C4156B59-DD7E-40DF-AF08-E568A27A6409}
+ 
