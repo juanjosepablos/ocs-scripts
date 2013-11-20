159b339ebba7d5310e180aa394489594b926604d
Juan Jose Pablos <juanjo@apertus.es>
2013-11-20 02:43:26 +0100

    add extension

--- limpiamierda/google-toolbar.bat
+++ limpiamierda/google-toolbar.bat
@@ -1,3 +1,3 @@
 ::Google Toolbar
-msiexec /qn /norestart /x {18455581-E099-4BA8-BC6B-F34B2F06600C}
+MsiExec.exe /qn /norestart /x {18455581-E099-4BA8-BC6B-F34B2F06600C}
 FOR /R "%PROGRAMFILES%\Google\Google Toolbar\Component\" %%V IN (GoogleToolbarManager_*.exe) DO %%V /uninstall
