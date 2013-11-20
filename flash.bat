f07497aec47fdeb2171e67c6d320694cad485a4e
Juan Jose Pablos <juanjo@apertus.es>
2013-11-20 23:07:36 +0100

    11.9.0.152

--- flash.bat
+++ flash.bat
@@ -7,7 +7,7 @@ echo AutoUpdateDisable=1 > %SYSTEMROOT%\System32\Macromed\Flash\mms.cfg
 
 :: ActiveX version for Internet Explorer
 
-install_flash_player_ax_11.9.900.117.exe -install
+install_flash_player_ax_11.9.900.152.exe -install
 
 :: Version for every other browser
-install_flash_player_11.9.900.117.exe -install
+install_flash_player_11.9.900.152.exe -install
