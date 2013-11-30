b02024b6b28f43b3fb8a7693b703ec558d6e8bc4
Juan Jose Pablos <juanjo@apertus.es>
2013-11-30 13:08:31 +0100

    x86 + x64

--- pandacloud.bat
+++ pandacloud.bat
@@ -1,9 +1,12 @@
 :: OPTIONAL: Panda Cloud
 
+IF [%PROCESSOR_ARCHITECTURE%]==[x86] SET PANDA_EXE=%TEMP%\PandaCloud\PandaCloudAntivirus_x86.msi
+IF [%PROCESSOR_ARCHITECTURE%]==[AMD64] SET PANDA_EXE=%TEMP%\PandaCloud\PandaCloudAntivirus_x64.msi
+
 del /Q /S %TEMP%\PandaCloud
 
 "%PROGRAMFILES%\7-Zip\7z" x -y -o"%TEMP%\PandaCloud" cloudantivirus-2.1.1.exe
 
-%TEMP%\PandaCloud\PandaCloudAntivirus_x86.msi /qn ANALISISDISABLE=1 INSTALLDIR="%PROGRAMFILES%\Panda Security\Panda Cloud Antivirus\"  ProductLanguage=1034 ALLOWINFOPANDA=true REBOOT=r SETUPEXEDIR=%TEMP%\PandaCloud\ CURRENTDIRECTORY=%TEMP%\PandaCloud
+%PANDA_EXE% /qn ANALISISDISABLE=1 INSTALLDIR="%PROGRAMFILES%\Panda Security\Panda Cloud Antivirus\"  ProductLanguage=1034 ALLOWINFOPANDA=true REBOOT=r SETUPEXEDIR=%TEMP%\PandaCloud\ CURRENTDIRECTORY=%TEMP%\PandaCloud
 
 
