dad243f186caab19f6b157cedb716c35e9c1b255
Juan Jose Pablos <juanjo@apertus.es>
2013-12-07 19:23:22 +0100

    add 2010 version

--- uninstall/bitdefender-uninstall.bat
+++ uninstall/bitdefender-uninstall.bat
@@ -1,2 +1,4 @@
 ::BitDefender Internet Security 2009
 msiexec /qn /norestart /x {27148014-3B0A-402B-8130-6B056357D12D}
+::BitDefender Internet Security 2010
+msiexec /qn /norestart /x {3E0E2640-A213-4BD2-A5FE-75FFA1EC1585}
