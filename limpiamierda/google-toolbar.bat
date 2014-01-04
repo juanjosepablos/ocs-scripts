6524ab8bfccbeece461abdddf1b0951fe2eab8b8
Juan Jose Pablos <juanjo@apertus.es>
2014-01-04 21:18:31 +0100

    exit terminal

--- limpiamierda/google-toolbar.bat
+++ limpiamierda/google-toolbar.bat
@@ -1,3 +1,4 @@
 ::Google Toolbar
 MsiExec.exe /qn /norestart /x {18455581-E099-4BA8-BC6B-F34B2F06600C}
 FOR /R "%PROGRAMFILES%\Google\Google Toolbar\Component\" %%V IN (GoogleToolbarManager_*.exe) DO %%V /uninstall
+exit
