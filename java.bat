d6d4c0115c6d1e0a076e9c1c62196761df892379
Juan Jose Pablos <juanjo@apertus.es>
2014-01-16 00:52:21 +0100

    fix eol

--- java.bat
+++ java.bat
@@ -109,10 +109,12 @@ taskkill /F /IM iexplore.exe /IM firefox.exe /IM jqs.exe
 %SYSTEMROOT%\SYSTEM32\msiexec /qn /norestart /x {26A24AE4-039D-4CA4-87B4-2F83217021FF}
 %SYSTEMROOT%\SYSTEM32\msiexec /qn /norestart /x {26A24AE4-039D-4CA4-87B4-2F83217025FF}
 %SYSTEMROOT%\SYSTEM32\msiexec /qn /norestart /x {26A24AE4-039D-4CA4-87B4-2F83217040FF}
+%SYSTEMROOT%\SYSTEM32\msiexec /qn /norestart /x {26A24AE4-039D-4CA4-87B4-2F83217045FF}
+%SYSTEMROOT%\SYSTEM32\msiexec /qn /norestart /x {26A24AE4-039D-4CA4-87B4-2F83217051FF}
 
 :: Then install latest Java 1.7
 ::INSTALL_JAVA 
-jre-7u45-windows-i586.exe /s /L %TEMP%\jre-7u45.log  REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0
+jre-7u51-windows-i586.exe /s /L %TEMP%\jre-7u51.log  REBOOT=ReallySuppress JAVAUPDATE=0 WEBSTARTICON=0
 
 :: Uninstall Java AutoUpdate
 %SYSTEMROOT%\SYSTEM32\msiexec /qn /x {4A03706F-666A-4037-7777-5F2748764D10}
@@ -120,3 +122,4 @@ jre-7u45-windows-i586.exe /s /L %TEMP%\jre-7u45.log  REBOOT=ReallySuppress JAVAU
 ::IF NOT EXIST %PROGRAMFILES%\Java\jre7\bin\java.exe GOTO INSTALL_JAVA
 :: Tell inventory to update
 "%PROGRAMFILES%\OCS Inventory Agent\OCSInventory.exe"  /force
+"%PROGRAMFILES(x86)%\OCS Inventory Agent\OCSInventory.exe"  /force
