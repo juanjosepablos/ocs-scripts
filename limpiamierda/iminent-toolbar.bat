49d65373b156f44b86e95d463449761ab63369ee
Juan Jose Pablos <juanjo@apertus.es>
2014-01-20 23:03:14 +0100

    another call

--- limpiamierda/iminent-toolbar.bat
+++ limpiamierda/iminent-toolbar.bat
@@ -1,3 +1,4 @@
 MsiExec.exe /qn /norestart /x {81FCC50B-950F-4063-8E4A-D99CAA4FBB1F}
 MsiExec.exe /qn /norestart /x {A76AA284-E52D-47E6-9E4F-B85DBF8E35C3}
+"%PROGRAM_FILES%\Iminent\inst\Bootstrapper\IminentUninstall.exe" /uninstall
 exit
