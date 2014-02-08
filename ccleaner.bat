84da61a095bedf147be7305c6b025c8b61cc2e27
Juan Jose Pablos <juanjo@apertus.es>
2014-02-08 14:04:00 +0100

    update version 4.10

--- ccleaner.bat
+++ ccleaner.bat
@@ -1,6 +1,6 @@
 ::OPTIONAL: Install Ccleaner
 ::HOME: http://www.piriform.com/CCLEANER
 
-ccsetup409.exe /S
+ccsetup410.exe /S
 "%ProgramFiles%\CCleaner\CCleaner.exe" /AUTO
 IF EXIST  "%allusersprofile%\Escritorio\CCleaner.lnk" del "%allusersprofile%\Escritorio\CCleaner.lnk"
