94b2042731775c9b51728585480334c107b1b55c
Juan Jose Pablos <juanjo@apertus.es>
2014-02-08 14:55:30 +0100

    up 5.8

--- win_malicious-software-removal-tool.bat
+++ win_malicious-software-removal-tool.bat
@@ -1,4 +1,4 @@
 :: Install the Microsoft Windows Malicious Software Removal Tool (KB890830)
 
-windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.7.exe /Q
+windows-kb890830-%PROCESSOR_ARCHITECTURE%-v5.8.exe /Q
 
