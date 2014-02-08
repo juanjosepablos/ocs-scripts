3ef206d7f1ccfdf90b76f6392bc0bbec4fa289b8
Juan Jose Pablos <juanjo@apertus.es>
2014-02-08 19:45:59 +0100

    +programs

--- limpiamierda/limpiainicio.bat
+++ limpiamierda/limpiainicio.bat
@@ -2,6 +2,7 @@
 reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v ares
 ::Messenger
 reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v MsnMsgr
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "MSMSGS"
 ::Google Notifier
 reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v swg
 ::Google Update
@@ -10,3 +11,5 @@ reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "Google Upda
 reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "NeroFilterCheck"
 ::Nokia Suite
 reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "PC Suite Tray"
+::CFTMOM
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "CTFMON.EXE"
