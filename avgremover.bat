0594fe4328aa7dc8271a147785a9bbc39cf0b8ac
Juan Jose Pablos <juanjo@apertus.es>
2014-02-12 15:41:31 +0100

    up 2014

--- avgremover.bat
+++ avgremover.bat
@@ -1,4 +1,5 @@
 :: OPTIONAL: AVG Anti-Virus Free Edition 07-Feb-2013
 :: HOME:http://free.avg.com/es-es/descargar.prd-afh#tba1
 
-avg_remover_stf_x86_2013_2706.exe /silent
+
+avg_remover_stf_%PROCESSOR_ARCHITECTURE%_2014_4116.exe /silent
