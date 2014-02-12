a0dacbc1aecd0b66a844f77e345d8a0a7c3fab42
Juan Jose Pablos <juanjo@apertus.es>
2014-02-12 16:09:09 +0100

    libreoffice -

---
+++ uninstall/libreoffice-uninstall.bat
@@ -0,0 +1,15 @@
+:: 4.1.X
+msiexec /qn /norestart /x {94E11973-ED58-47A0-907C-ABF6D95C5DD8}
+msiexec /qn /norestart /x {4F3722AD-197D-4DBB-BDFB-D2F0D6776354} 
+msiexec /qn /norestart /x {DD3CB916-F91A-41B9-B276-CAC090E91021} 
+msiexec /qn /norestart /x {F1EE568A-171F-4C06-9BE6-2395BED067A3} 
+msiexec /qn /norestart /x {F8478020-D98E-49FB-BA14-07A534AED99C} 
+msiexec /qn /norestart /x {E77773E5-944A-453F-97F3-46767AE0A253}
+:: 4.0.X
+msiexec /qn /norestart /x {85595843-720E-4344-8210-F1ACAE87B459} 
+msiexec /qn /norestart /x {5B9C9486-4287-4621-8F9D-EC3EE622A82F}
+msiexec /qn /norestart /x {F77ED0CD-2E5E-4FC7-82E0-BB7D461E739F}
+msiexec /qn /norestart /x {F77ED0CD-2E5E-4FC7-82E0-BB7D461E739F}
+msiexec /qn /norestart /x {1062AD6C-80F4-4BC6-AB7C-A28892B497B8}
+msiexec /qn /norestart /x {604B2A5C-B1CE-45B2-ADCC-6B7C721AC3AC} 
+msiexec /qn /norestart /x {8EA569F1-97AF-4C3E-A0CB-4846C2D35A81}
