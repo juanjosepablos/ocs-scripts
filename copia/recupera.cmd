b26909a9f3750b80111dedc523cfd76a68b14c26
Juan Jose Pablos <juanjo@apertus.es>
2014-06-17 23:35:41 +0200

    +recupera

---
+++ copia/recupera.cmd
@@ -0,0 +1,71 @@
+@ECHO OFF
+REM *****************************************************************
+REM
+REM CWRSYNC.CMD - Batch file template to start your rsync command (s).
+REM
+REM By Tevfik K. (http://itefix.no)
+REM *****************************************************************
+
+REM Make environment variable changes local to this batch file
+SETLOCAL
+
+REM ** CUSTOMIZE ** Specify where to find rsync and related files (C:\CWRSYNC)
+SET CWRSYNCHOME=C:\COPIA
+
+REM Set CYGWIN variable to 'nontsec'. That makes sure that permissions
+REM on your windows machine are not updated as a side effect of cygwin
+REM operations.
+SET CYGWIN=nontsec
+
+REM Set HOME variable to your windows home directory. That makes sure 
+REM that ssh command creates known_hosts in a directory you have access.
+SET HOME=%HOMEDRIVE%%HOMEPATH%
+
+REM Make cwRsync home as a part of system PATH to find required DLLs
+SET CWOLDPATH=%PATH%
+SET PATH=%CWRSYNCHOME%\BIN;%PATH%
+
+REM Windows paths may contain a colon (:) as a part of drive designation and 
+REM backslashes (example c:\, g:\). However, in rsync syntax, a colon in a 
+REM path means searching for a remote host. Solution: use absolute path 'a la unix', 
+REM replace backslashes (\) with slashes (/) and put -/cygdrive/- in front of the 
+REM drive letter:
+REM 
+REM Example : C:\WORK\* --> /cygdrive/c/work/*
+REM 
+REM Example 1 - rsync recursively to a unix server with an openssh server :
+REM
+REM       rsync -r /cygdrive/c/work/ remotehost:/home/user/work/
+REM
+REM Example 2 - Local rsync recursively 
+REM
+REM       rsync -r /cygdrive/c/work/ /cygdrive/d/work/doc/
+REM
+REM Example 3 - rsync to an rsync server recursively :
+REM    (Double colons?? YES!!)
+REM
+REM       rsync -r /cygdrive/c/doc/ remotehost::module/doc
+REM
+
+SET RSYNC_BIN=%SYSTEMDRIVE%\copia\bin\rsync.exe
+:: CONVERT \ TO /
+SET USERHOME=%HOMEPATH%
+SET USERHOME=%USERHOME:\=/%
+SET EXTERNO=F
+
+:: NETWORK SHARE
+NET USE %EXTERNO%: \\casa\tmp
+
+::CREATE A FOLDER FOR THE COMPUTER
+IF NOT EXIST %EXTERNO%:\%COMPUTERNAME% MD %EXTERNO%:\%COMPUTERNAME%
+
+SET DENTRO=/cygdrive/c%USERHOME%/
+IF NOT EXIST %DENTRO% EXIT /B
+
+SET FUERA=/cygdrive/%EXTERNO%/%COMPUTERNAME%/%USERNAME%/
+IF NOT EXIST %FUERA% EXIT /B
+
+
+%RSYNC_BIN% -av --chmod=ugo=rwX --delete --modify-window=1 --include-from=lista.txt "%FUERA%" "%DENTRO%"
+
+
