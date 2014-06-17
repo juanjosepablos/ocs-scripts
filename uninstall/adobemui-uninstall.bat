a4803bb8360c34126e2272f17b59b9f68ccada54
Juan Jose Pablos <juanjo@apertus.es>
2014-06-17 23:31:11 +0200

    +adobeui +office 2010 uninstall

---
+++ uninstall/adobemui-uninstall.bat
@@ -0,0 +1,6 @@
+:: 10.1.x
+MsiExec.exe /qn /norestart /x {AC76BA86-7AD7-1034-7B44-AA1000000001}
+:: 11.0.X
+MsiExec.exe /qn /norestart /x {AC76BA86-7AD7-1034-7B44-AB0000000001}
+:: 11.0.X MUI
+MsiExec.exe /qn /norestart /x {AC76BA86-7AD7-FFFF-7B44-AB0000000001}
---
+++ uninstall/office2010-uninstall.bat
@@ -0,0 +1,5 @@
+::Uninstall Office 2010
+::http://download.microsoft.com/download/0/e/d/0eda9ae6-f5c9-44be-98c7-ccc3016a296a/ork.exe
+MsiExec.exe /qn /norestart /x {95140000-0070-0000-0000-0000000FF1CE}
+MsiExec.exe /qn /norestart /x {90140000-006D-0C0A-1000-0000000FF1CE}
+MsiExec.exe /qn /norestart /x {90140011-0066-0C0A-0000-0000000FF1CE}
